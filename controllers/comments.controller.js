7b4d7682f2eaff8e8ea51a99fea11810f0c54b01
Prince Singh <marvelousprince012233@gmail.com>
2023-08-12 03:47:29 +0530

    piston API

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,48 +1,36 @@
+const axios = require("axios");
+
 const commentsHandler = async (context) => {
   console.log("\ncommentsHandler\n");
 
-  const { owner, repo, issue_number } = context.issue();
-  const octokit = context.octokit;
   console.log({ owner, repo, issue_number });
 
   // Check if the comment was posted on a pull request
   if (context.payload.issue.pull_request) {
-    const pullRequest = await octokit.rest.pulls.get({
-      owner,
-      repo,
-      pull_number: issue_number,
-    });
-    console.log({ pullRequest });
-
+    const comment = context.payload.comment.body;
     const commandRegex = /\/execute/i;
-    const hasExecuteCommand = commandRegex.test(context.payload.comment.body);
-    console.log(context.payload.comment.body);
+    const hasExecuteCommand = commandRegex.test(comment);
+    console.log(comment);
 
     if (hasExecuteCommand) {
-      const code = context.payload.comment.body;
-      console.log(code);
-
-      // try {
-      //   const pistonClient = new piston.Client();
-      //   const executionResult = await pistonClient.execute(code);
-
-      //   // Post the execution result as a reply to the comment
-      //   await octokit.rest.issues.createComment({
-      //     owner,
-      //     repo,
-      //     issue_number,
-      //     body: `Execution Result:\n\`\`\`\n${executionResult}\n\`\`\``,
-      //   });
-      // } catch (error) {
-      //   // Handle any errors during code execution
-      //   console.error("Error executing code:", error);
-      //   await octokit.rest.issues.createComment({
-      //     owner,
-      //     repo,
-      //     issue_number,
-      //     body: `Error executing code:\n\`\`\`\n${error.message}\n\`\`\``,
-      //   });
-      // }
+      const pullRequestFiles = await context.octokit.rest.pulls.listFiles({
+        owner,
+        repo,
+        pull_number: issue_number,
+      });
+      for (const file of pullRequestFiles.data) {
+        if (file.filename == "test.py") {
+          const response = await axios.post(
+            "https://emkc.org/api/v2/piston/execute",
+            {
+              language: "python",
+              source: file.patch,
+              version: "3.9.6",
+            }
+          );
+          console.log(response.data);
+        }
+      }
     }
   }
 };
