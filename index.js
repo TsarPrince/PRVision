567d66038e908ee4f91e81ce985df99b746bd51f
Prince Singh <marvelousprince012233@gmail.com>
2023-08-12 00:52:29 +0530

    fetch PR files

--- index.js
+++ index.js
@@ -2,19 +2,39 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
 
+module.exports = (app) => {
   app.on(
     ["pull_request.opened", "pull_request.edited", "pull_request.reopened"],
     async (context) => {
-      if (context.payload.pull_request.title.indexOf("🤖") > -1) {
-        await context.octokit.pulls.createReview({
-          ...context.pullRequest(),
-          event: "APPROVE",
-        });
+      const { owner, repo } = context.issue();
+      console.log({ owner, repo });
+
+      const rootContent = await context.octokit.rest.repos.getContent({
+        owner,
+        repo,
+        // path,
+      });
+      console.log(rootContent);
+      for (const content of rootContent.data) {
+        console.log(content);
+        if (content.type === "file" && content.name === "code.js") {
+          const testFile = await context.octokit.rest.repos.getContent({
+            owner,
+            repo,
+            path: content.path,
+          });
+          console.log(testFile);
+        }
       }
+
+      // if (context.payload.pull_request.title.indexOf("🤖") > -1) {
+      //   // context.octokit gives us access to an authenticated octokit/rest.js client to make GitHub API calls with ease.
+      //   await context.octokit.pulls.createReview({
+      //     ...context.pullRequest(),
+      //     event: "APPROVE",
+      //   });
+      // }
     }
   );
 
