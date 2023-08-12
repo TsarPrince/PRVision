c96674a31fcd9df27783987b152982a0ee8668f0
Prince Singh <marvelousprince012233@gmail.com>
2023-08-12 08:54:32 +0530

    /execute code/print.py

--- code/print.py
+++ code/print.py
@@ -1,4 +1,2 @@
 for i in range(10):
     print('🐱' * 10)
-
-
--- utils/index.js
+++ utils/index.js
@@ -10,7 +10,7 @@ const getRuntime = (extension) => {
 };
 
 const searchExecuteCmd = (message) => {
-  // split the comment with whitespaces
+  // split the message with whitespaces
   // ASSUMPTION: /execute command is always followed by a filename
   const indexOfExecuteCmd = message.split(/\s+/).indexOf("/execute");
   const filename = message.split(/\s+/)[indexOfExecuteCmd + 1];
@@ -104,7 +104,7 @@ const findFileAndExecute = async (
       owner,
       repo,
       issue_number,
-      body: `File \`${commentFileName}\` not found in this pull request. Available files are:\n\`\`\`\n${files.data.reduce(
+      body: `File \`${inputFileName}\` not found in this pull request. Available files are:\n\`\`\`\n${files.data.reduce(
         (accumulator, current) => accumulator + current.filename + "\n",
         ""
       )}\n\`\`\``,
